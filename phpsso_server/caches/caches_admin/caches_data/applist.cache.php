d21cd0f7e98ef25e096463b867e08f69e7777852
wodrow <wodrow451611cv@gmail.com>
2016-08-13 17:03:03 +0800

    init

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/phpcms/',
+    'authkey' => 'DxygfZbl9EEl7hBaMiCg0LhsFEpdwYSe',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
