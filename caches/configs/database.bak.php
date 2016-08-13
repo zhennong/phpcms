1a142737bf1393f72d22861012226fe778b3741f
wodrow <wodrow451611cv@gmail.com>
2016-08-13 17:46:08 +0800

    init

---
+++ caches/configs/database.bak.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/8/13
+ * Time: 17:45
+ */
+
+return array (
+    'default' => array (
+        'hostname' => '',
+        'port' => 3306,
+        'database' => 'phpcmsv9',
+        'username' => '',
+        'password' => '',
+        'tablepre' => 'v9_',
+        'charset' => 'utf8',
+        'type' => 'mysqli',
+        'debug' => true,
+        'pconnect' => 0,
+        'autoconnect' => 0
+    ),
+);
